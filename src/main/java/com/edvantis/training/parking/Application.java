8669ae685d2d984d54da8a6c081de8292377545a
taras.fihurnyak <adrenalinchik@gmail.com>
2017-02-15 12:07:37 +0200

    Main start application

--- src/main/java/com/edvantis/training/parking/Application.java
+++ src/main/java/com/edvantis/training/parking/Application.java
@@ -1,12 +1,18 @@
 package com.edvantis.training.parking;
 
+import com.edvantis.training.parking.jdbc.ParkingJdbcServiceImpl;
+
 /**
  * Created by taras.fihurnyak on 2/9/2017.
  */
 public class Application {
     public static void main(String[] args) {
 
+        ParkingJdbcServiceImpl jdbcService = new ParkingJdbcServiceImpl();
+        //jdbcService.createDb("test","root","root");
+
         if (args.length > 0) {
+            jdbcService.createDb(args[0],args[1],args[2]);
 
         }
     }
